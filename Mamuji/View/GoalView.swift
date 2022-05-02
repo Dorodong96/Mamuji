dafa5e262aa10dc1402036f5e4f464eec66f6c5d
DK <dk143496@gmail.com>
2022-05-03 01:22:26 +0900

    move: Move to Group

---
+++ Mamuji/View/GoalView.swift
@@ -0,0 +1,20 @@
+//
+//  GoalView.swift
+//  Mamuji
+//
+//  Created by DongKyu Kim on 2022/04/30.
+//
+
+import SwiftUI
+
+struct GoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalView()
+    }
+}
