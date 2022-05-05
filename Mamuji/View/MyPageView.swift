f3e4ee04b16fc748cae11f05f89f63f50c8c1126
DK <dk143496@gmail.com>
2022-05-06 01:20:57 +0900

    feat: add dummy data, status bar can show status of dummy data

--- Mamuji/View/MyPageView.swift
+++ Mamuji/View/MyPageView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct MyPageView: View {
     private var columns = Array(repeating: GridItem(.flexible(minimum: 80, maximum: 170), spacing: 0), count: 2)
     
+    // dummyData
+    @State var totalTask = 120
+    @State var checkedTask = 45
+    
     var body: some View {
         VStack(spacing: 0) {
             Text("마이페이지")
@@ -33,11 +37,26 @@ struct MyPageView: View {
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding()
         
-        RoundedRectangle(cornerRadius: 10)
-            .frame(width: 352, height: 50, alignment: .center)
-            .foregroundColor(.gray)
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                .frame(width: 352, height: 50, alignment: .center)
+            .foregroundColor(Color(UIColor.systemGray5))
+                
+                
+                HStack(spacing:0) {
+                    Color(UIColor.systemGray2)
+                        .cornerRadius(8, corners: [.topLeft, .bottomLeft])
+                    .frame(width: CGFloat(checkedTask * 346 / totalTask), height: 42, alignment: .leading)
+                    Color(UIColor.systemGray5)
+                        .cornerRadius(10, corners: [.topRight, .bottomRight])
+                    .frame(width: CGFloat(346-(checkedTask * 346 / totalTask)), height: 42, alignment: .leading)
+                }
+                
+                Text("\(checkedTask * 100 / totalTask)%")
+                    .font(.custom("BMDoHyeon-OTF", size: 18))
+            }
         
-        Text("조금만 더 열심히 해보자!")
+        Text("잘 하고 있지 우리 아들 ~ ^_^")
             .font(.custom("BMDoHyeon-OTF", size: 20))
             .frame(maxWidth: .infinity, alignment: .center)
             .foregroundColor(.gray)
@@ -82,5 +101,23 @@ struct MyPageView: View {
 struct MyPageView_Previews: PreviewProvider {
     static var previews: some View {
         MyPageView()
+    }
+}
+
+// Clipped Rounded Rectangle
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+// View Extension for cornerRadius
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
     }
 }
